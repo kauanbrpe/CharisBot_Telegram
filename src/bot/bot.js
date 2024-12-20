65adf9883476a8a6d74d387c4ed9500519a11ab1
mayconyrp <csa.devmaycon@gmail.com>
2024-12-20 00:07:22 -0300

    funcao msg automatizada

--- src/bot/bot.js
+++ src/bot/bot.js
@@ -8,4 +8,22 @@ const bot = new Telegraf(BOT_TOKEN);
 // Carrega os comandos e eventos do bot
 loadCommands(bot);
 
+// Função para enviar mensagem automática para o canal
+function sendAutomaticMessage() {
+    const CHANNEL_ID = -1002384781900;  // ou use o chat_id do canal privado
+    const message = 'Lembrete: Caderneta - IMIP!';
+
+    bot.telegram.sendMessage(CHANNEL_ID, message)
+        .then(() => {
+            console.log('Mensagem enviada com sucesso!');
+        })
+        .catch((error) => {
+            console.error('Erro ao enviar mensagem automática:', error);
+        });
+}
+
+// Envia a mensagem automaticamente a cada 1 hora (3600000 ms)
+setInterval(sendAutomaticMessage, 3600000);
+
+// Exporte o bot para outros arquivos, se necessário
 module.exports = bot;
