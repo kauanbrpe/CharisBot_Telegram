8fcc33d974601eb1c509e53c0be4405acf3a5b29
mayconyrp <csa.devmaycon@gmail.com>
2024-12-20 11:09:36 -0300

    correção: duplicacao do sétimo domingo - sao jose

--- src/bot/handlers/SaoJose/oracoes.js
+++ src/bot/handlers/SaoJose/oracoes.js
@@ -29,7 +29,6 @@ const handleOracaoJosefina = (ctx) => {
 const handleSeteDomingos = async (ctx) => {
     const oracao = oracoesSaoJose.seteDomingos;
 
-    // Função auxiliar para enviar mensagens com atraso
     const sendMessageWithDelay = (message, delay) => {
         return new Promise(resolve => setTimeout(() => {
             ctx.reply(message);
@@ -37,16 +36,13 @@ const handleSeteDomingos = async (ctx) => {
         }, delay));
     };
 
-    // Enviar todos os 6 primeiros domingos com atraso
     for (let i = 0; i < 6; i++) {
         const message = `${oracao[i].titulo_pt}\n\n${oracao[i].conteudo_pt}`;
         await sendMessageWithDelay(message, 1000); // 1000ms = 1 segundo de atraso
     }
 
-    // Enviar o sétimo domingo com atraso e botão de voltar
     const message7 = `${oracao[6].titulo_pt}\n\n${oracao[6].conteudo_pt}`;
-    await sendMessageWithDelay(message7, 1000); // 1000ms = 1 segundo de atraso
-    sendWithBackButton(ctx, message7, []); // Enviar a mensagem com o botão de voltar
+    sendWithBackButton(ctx, message7, []); // Esta linha já envia a mensagem corretamente com o botão
 };
 
 module.exports = {
